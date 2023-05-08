2f175170df0caafb492a68319a79c9f41eeb6ebe
hxj5 <hxj5@hku.hk>
2023-05-08 14:55:41 +0800

    utils: init gen_xclbm_fast2

---
+++ xclbm/scripts/utils/gen_xclbm_fast2.run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# gen_xclbm_fast2.run.sh
+
+
+repo_scripts_dir=/home/xianjie/projects/CNV_calling_Benchmark/xclbm/scripts
+gene_anno=/groups/cgsd/xianjie/data/refapp/xclone/annotate_genes_hg38_update.txt
+result_dir=/home/xianjie/debug/test-xclbm/fast2
+
+
+if [ ! -e "$result_dir" ]; then
+    mkdir $result_dir
+fi
+
+
+echo -e "\nGenerate xclbm scripts for gene scale.\n"
+out_dir=$result_dir/gene_scale
+python gen_xclbm_fast2.py  \
+  --sid  GX109   \
+  --sp  GX109        \
+  --cnvScale  gene    \
+  --outdir  $out_dir    \
+  --datList  /home/xianjie/debug/test-xclbm/normal/gene_scale  \
+  --xclone  /groups/cgsd/xianjie/result/xclbm/GX109/GX109_5400_xclone_0403/extracted_data \
+  --geneAnno  $gene_anno     \
+  --repoScripts  $repo_scripts_dir   \
+  --plotDec  4
+
+chmod u+x $out_dir/run.sh
+
+
+echo -e "\nGenerate xclbm scripts for arm scale.\n"
+out_dir=$result_dir/arm_scale
+python gen_xclbm_fast2.py  \
+  --sid  GX109   \
+  --sp  GX109        \
+  --cnvScale  arm    \
+  --outdir  $out_dir    \
+  --datList  /home/xianjie/debug/test-xclbm/normal/arm_scale  \
+  --xclone  /groups/cgsd/xianjie/result/xclbm/GX109/GX109_5400_xclone_0403/extracted_data_merge_chr_arm    \
+  --geneAnno  $gene_anno      \
+  --repoScripts  $repo_scripts_dir   \
+  --plotDec  4
+
+chmod u+x $out_dir/run.sh
+
